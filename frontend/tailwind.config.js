e17dc9d17b87ef0c5aa8cf4d6e160a61b1e293c9
HackerManOSU <130389151+HackerManOSU@users.noreply.github.com>
2025-01-30 22:56:30 +0000

    Tailwing Config Color Palettes 🎨

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#66b2b2',
+          light: '#f0f9f9',
+          dark: '#fff',
+        },
+        secondary: {
+          DEFAULT: '#f0f9f9',
+          light: '#fff',
+          dark: '#fff',
+        },
+        tertiary: {
+          DEFAULT: '#2c3e50',
+          light: '#fff',
+          dark: '#fff',
+        },
+      },
+    },
   },
   plugins: [],
 }
