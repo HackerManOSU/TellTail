c63e3db2f7196bd4c57c09a8c5e8f1ae076fbfd3
newnate2005 <newnate2005@gmail.com>
2025-01-30 15:51:34 -0800

    updated about us page

--- frontend/src/components/About Us/AboutUs.tsx
+++ frontend/src/components/About Us/AboutUs.tsx
@@ -2,6 +2,33 @@ const AboutUs = () => {
     return (
       <div className="max-w-6xl mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold text-primary mb-6">About Us</h1>
+        
+        <p className="text-lg text-gray-700 mb-4">
+          Welcome to our site! We are dedicated to helping you learn more about your pet, 
+          from its breed to other potentional insights!
+        </p>
+        <h2 className="text-2xl font-semibold text-primary mt-6 mb-4">Our Mission</h2>
+        <p className="text-lg text-gray-700 mb-4">
+          Our mission is to provide a quick and easy way to learn more about your
+          cat or dog. We aim to provide accurate information on your pet's breed, 
+          age, and potential health information.
+        </p>
+        <h2 className="text-2xl font-semibold text-primary mt-6 mb-4">Our Team</h2>
+        <p className="text-lg text-gray-700 mb-4">
+          We are a group of students at Oregon State University with diverse backgrounds, 
+          all working together to achieve a common goal.
+        </p>
+        <ul className="text-lg text-gray-700 mb-4">
+          <li>Sam Neale</li>
+          <li>Raed Kabir</li>
+          <li>Christian DeVore</li>
+          <li>Nathaniel White</li>
+          <li>Stephen Tsui</li>
+          <li>Troy Diaz</li>
+          <li>Zane Garvey</li>
+        </ul>
+      
+      
       </div>
     )
   }
